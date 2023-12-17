d92cf4f6a33666d543e848f7985e7de30b2d2858
Trinh Thai Duong <91010847+dwgth4i@users.noreply.github.com>
2023-12-18 00:03:31 +0700

    Update theweakend.py

--- theweakend.py
+++ theweakend.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 import sys
 from itertools import permutations
+import argparse
+
 
 results = []
 salt_list = ["1", "69","123", "420", "12345", "69420", "123456", "1337", "xinh", "cute","dangiu", "deptrai", "deptraivcl", "vip", "pro", "vippro", "!", "@", "$"]
 
+
 # worker
-def generator_for_fullname(perm,dob, results):
+def generator_for_fullname(perm, dob, results):
     for i in perm:
         for j in salt_list:
             pw1 = ''.join(i).lower()
@@ -47,19 +50,19 @@ def generator_for_nickname(nickname, dob, results):
     pw7 = nickname + str(dob[2])
     pw8 = pw6 + str(dob[2][-2]) + str(dob[2][-1])
     pw9 = nickname + str(dob[2][-2]) + str(dob[2][-1])
-
     results.append(pw5)
     results.append(pw6)
     results.append(pw7)
     results.append(pw8)
     results.append(pw9)
-# password generator function that based on the fullname and dob provided
-def name_and_dob_based_gen():
-    stack1 = sys.argv[1].split(" ")
+    
+# password generator function that is based on the fullname and dob provided
+def name_and_dob_based_gen(fullname, dob):
+    stack1 = fullname.split(" ")
     stack2 = [stack1[-2], stack1[-1]]
     stack3 = [stack1[0], stack1[-1]]
     stack4 = [stack1[0][0], stack1[-2][0], stack1[-1]]
-    dob = sys.argv[3].split("-")
+    dob = dob.split("-")
     perm1 = list(permutations(stack1))
     perm2 = list(permutations(stack2))
     perm3 = list(permutations(stack3))
@@ -69,19 +72,13 @@ def name_and_dob_based_gen():
     generator_for_fullname(perm3, dob, results)
     generator_for_fullname(perm4, dob, results)
   
-
 # password generator function that based on the nickname and dob provided
-def nickname_and_dob_based_gen():
-    nickname = sys.argv[2]
-    dob = sys.argv[3].split("-")
+def nickname_and_dob_based_gen(nickname, dob):
+    dob = dob.split("-")
     generator_for_nickname(nickname,dob, results)
 
-# password generator function that based on the fullname and the nickname provided
-def name_and_nickname_based_gen():
-    pass
-
 def main():
-    # Banner ngầu vl
+
     banner = r"""_________          _______             _______  _______  _          _______  _        ______  
 \__   __/|\     /|(  ____ \  |\     /|(  ____ \(  ___  )| \    /\  (  ____ \( (    /|(  __  \ 
    ) (   | )   ( || (    \/  | )   ( || (    \/| (   ) ||  \  / /  | (    \/|  \  ( || (  \  )
@@ -93,34 +90,53 @@ def main():
 """
 
     #print usage if improper # of args
-    if len(sys.argv) == 1 or len(sys.argv) != 4:
+
+    if len(sys.argv) == 1 or len(sys.argv) < 5:
         print(banner)
-        print('[+] Cách sử dụng: ./theweakend.py <full name> <nickname> <dob(date-month-year)>')
-        print('[+] Ví dụ: ./theweakend.py "Le Van A" "ale123" "1-1-2000"')
-        print('[+] Nếu không có nickname hay tên gọi khác, bạn có thể điền vào kí tự khoảng trắng điền như này " "')
+        print('[+] Usage: python3 theweakend.py [-f|--fullname] <Họ và tên> [-d|--date-of-birth] <ngày-tháng-năm>')
+        print('Options:')
+        print(' -f fullname, --fullname fullname')
+        print('         Tên đầy đủ của đối tượng (bắt buộc)')
+        print(' -d dob, --date-of-birth')
+        print('         Ngày tháng năm sinh của đối tượng (bắt buộc)')
+        print(' -n nickname --nickname nickname')
+        print('         Nickname hoặc tên gọi khác của đối tượng')
+        print(' -c      Thêm kết quả cho viết hoa chữ cái đầu')
         quit()
 
+    parser = argparse.ArgumentParser(description=banner)
+
+    parser.add_argument("-f", "--fullname", dest="fullname", required=True)
+    parser.add_argument("-d", "--date-of-birth", dest="dob", required=True)
+    parser.add_argument("-n", "--nickname", dest="nickname", required=False)
+    parser.add_argument("-c", "--case-sensitive", dest="case", required=False, action="store_true")
+
+    args = parser.parse_args()
+    fullname = args.fullname
+    dob = args.dob
+    nickname = args.nickname
+    case = args.case
+
+
     #quit if fullname is only a single word
-    if len(sys.argv[1].split(" ")) == 1:
+    if len(fullname.split(" ")) == 1:
         print("[-] Họ tên gì chỉ một từ thế bro ?")
-        quit()
-    if sys.argv[2] == " ":
-        print("[+] Đợi chút nha bro :D", end="\r")
-        file_name = "passwords.txt"
-        name_and_nickname_based_gen()
-        with open(file_name, 'w') as file:
-            for pw in list(set(results)):
-                file.write(pw + "\n")
-        print("[+] Done, enjoy <3 ")
         quit()
 
     file_name = "passwords.txt"
     print("[+] Đợi chút nha bro :D")
-    name_and_dob_based_gen()
-    nickname_and_dob_based_gen()
+    name_and_dob_based_gen(fullname=fullname, dob=dob)
+    if nickname:
+        nickname_and_dob_based_gen(nickname=nickname, dob=dob)
     with open(file_name, 'w') as file:
         for pw in list(set(results)):
             file.write(pw + "\n")
+    
+    if case:
+        with open(file_name, 'a') as file:
+            for pw in list(set(results)):
+                file.write(pw.capitalize() + "\n")
+
     print("[+] Done, enjoy <3 ")
     quit()
 
